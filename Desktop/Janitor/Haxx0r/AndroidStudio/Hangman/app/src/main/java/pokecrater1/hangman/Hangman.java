f0299fbc401e6ce7ec09954458021bf65b0f1f2b
Vy Phan <pokecrater1@utexas.edu>
2016-11-12 15:45:42 -0600

    Add files via upload

--- Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
+++ Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
+import android.view.View.OnKeyListener;
 
 public class Hangman extends AppCompatActivity {
 
@@ -15,12 +16,15 @@ public class Hangman extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
-        Button button1 = (Button)this.findViewById(R.id.click_btn);
-        button1.setOnClickListener(new Button.OnClickListener() {
+        Button b = (Button)this.findViewById(R.id.button1);
+        b.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v)  {
-
+                TextView resp = (TextView) findViewById(R.id.response);
+                EditText name = (EditText) findViewById(R.id.textView);
+                String str = "Welcome " + name.getText().toString() + " !";
+                resp.setText(str);
             }
-        }
+        });
     }
 }
 
