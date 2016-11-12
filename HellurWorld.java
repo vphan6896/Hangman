227a764253dc72d8650356afa3f8f7a5eadb1b5e
Vy Phan <pokecrater1@utexas.edu>
2016-11-12 13:58:07 -0600

    Add files via upload

---
+++ HellurWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HellurWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		String name = scanner.nextLine();
+		System.out.println("Hellur " + name);
+
+	}
+
+}
