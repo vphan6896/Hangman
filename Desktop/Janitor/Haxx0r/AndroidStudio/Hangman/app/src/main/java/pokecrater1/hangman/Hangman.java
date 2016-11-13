d6c8847d27254588ba970e961d8a335319b037dc
Vy Phan <pokecrater1@utexas.edu>
2016-11-12 21:15:15 -0600

    Add files via upload

--- Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
+++ Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
@@ -14,38 +14,90 @@ import java.util.Random;
 
 public class Hangman extends AppCompatActivity {
     String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    TextView initialABC;
     String[] movieList = new String[20];
-    String word = movieList(movieList);
+    String word = movieList(movieList).toUpperCase();
+    TextView phrase;
     String underScores = "";
-
+    String newUnderScores = "";
+    int counter;
+    TextView numWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
         Button b = (Button) this.findViewById(R.id.button1);
-        b.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                TextView resp = (TextView) findViewById(R.id.response);
-                EditText input = (EditText) findViewById(R.id.textView);
-                String guess = input.getText().toString();
-                abc = changeABC(guess, abc);
-                resp.setText(abc);
-                // changes the word into underscores
-                underScores = underScores(word, underScores);
-                TextView phrase = (TextView) findViewById(R.id.phraseid);
-                phrase.setText(underScores);
-            }
-        });
+        initialABC = (TextView) this.findViewById(R.id.initialABC);
+        initialABC.setText(abc);
+        phrase = (TextView) this.findViewById(R.id.phraseid);
+        newUnderScores = underScores(word,underScores);
+        phrase.setText(newUnderScores);
+        numWrong = (TextView) findViewById(R.id.numWrong);
+        numWrong.setText("Number Wrong: " + counter);
+            b.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                        TextView resp = (TextView) findViewById(R.id.response);
+                        EditText input = (EditText) findViewById(R.id.textView);
+                        String guess = input.getText().toString();
+                        initialABC.setText("");
+                        abc = changeABC(guess, abc);
+                         resp.setText(abc);
+                        // changes the word into underscores
+                        newUnderScores = guessResults(guess, word, newUnderScores);
+                        TextView phrase = (TextView) findViewById(R.id.phraseid);
+                        phrase.setText(newUnderScores);
+                        counter = checkWinOrLose(guess, word, counter);
+                    TextView numWrong = (TextView) findViewById(R.id.numWrong);
+                    numWrong.setText("Number Wrong: " + counter);
+                    if(counter >= 5) {
+                        gameOver();
+                    }
+                }
+
+            });
+
     }
 
     public static String underScores(String word, String underScores) {
-        underScores = "";
-        for (int index = 0; index < word.length() - 1; index++) {
-            underScores += "_ ";
+        for (int index = 0; index < word.length(); index++) {
+            if(word.charAt(index) == ' ') {
+                underScores += "_";
+            }
+            else
+                underScores += "*";
         }
-        underScores += "_";
         return underScores;
+    }
+
+    // changes the current phrase to insert the guess if it matches a letter in
+    // the secret phrase
+    public static String newCurrentPhrase(String guess, String phrase, String newUnderScores) {
+        String result = "";
+
+        for (int i = 0; i < phrase.length(); i++) {
+            String letterString = phrase.charAt(i) + "";
+            if (letterString.equals(guess)) {
+                result += guess;
+            } else if (letterString.equals(" ")) {
+                result += "_";
+            } else {
+                result += newUnderScores.charAt(i);
+            }
+        }
+        return result;
+    }
+
+
+
+    // checks the guess and makes a new current phrase if the guess was a letter
+    // of the secret phrase
+    public static String guessResults(String guess, String secretPhrase, String currentPhrase) {
+        if (secretPhrase.contains(guess)) {
+            currentPhrase = newCurrentPhrase(guess, secretPhrase, currentPhrase);
+        } else {
+        }
+        return currentPhrase;
     }
 
     public static String movieList(String[] movieList) {
@@ -58,7 +110,7 @@ public class Hangman extends AppCompatActivity {
         movieList[6] = "The Wizard of Oz";
         movieList[7] = "Forrest Gump";
         movieList[8] = "Gladiator";
-        movieList[9] = "Titanc";
+        movieList[9] = "Titanic";
         movieList[10] = "Saving Private Ryan";
         movieList[11] = "Unforgiven";
         movieList[12] = "Raiders of the Lost Ark";
@@ -88,5 +140,19 @@ public class Hangman extends AppCompatActivity {
         return beginning + end;
     }
 
+    // checks to see if the guess was a letter of the secret phrase and then
+    // returns 1 if the guess is wrong or 0 if it is right
 
+    public static int checkWinOrLose(String guess, String phrase, int counter) {
+        if (!phrase.contains(guess)) {
+            counter++;
+        }
+        return counter;
+    }
+
+    //this is where the game ends
+    public void gameOver() {
+        Button b = (Button) this.findViewById(R.id.button1);
+        b.setEnabled(false);
+    }
 }
