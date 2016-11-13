bfa08b18b8654b6b8bc9446eb9782f7d2dfbfa9e
Vy Phan <pokecrater1@utexas.edu>
2016-11-13 11:04:16 -0600

    Add files via upload

--- Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
+++ Desktop/Janitor/Haxx0r/AndroidStudio/Hangman/app/src/main/java/pokecrater1/hangman/Hangman.java
@@ -3,7 +3,9 @@ package pokecrater1.hangman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
@@ -22,15 +24,19 @@ public class Hangman extends AppCompatActivity {
     String newUnderScores = "";
     int counter;
     TextView numWrong;
+    String status = "";
+    EditText input;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
+        b = (Button) this.findViewById(R.id.button1);
         initialABC = (TextView) this.findViewById(R.id.initialABC);
         initialABC.setText(abc);
         phrase = (TextView) this.findViewById(R.id.phraseid);
+        input = (EditText) findViewById(R.id.textView);
         newUnderScores = underScores(word, underScores);
         phrase.setText(newUnderScores);
         numWrong = (TextView) findViewById(R.id.numWrong);
@@ -50,6 +56,7 @@ public class Hangman extends AppCompatActivity {
                 setContentView(R.layout.activity_hangman);
                 initialABC = (TextView) findViewById(R.id.initialABC);
                 initialABC.setText(abc);
+                input = (EditText) findViewById(R.id.textView);
                 phrase = (TextView) findViewById(R.id.phraseid);
                 newUnderScores = underScores(word, underScores);
                 phrase.setText(newUnderScores);
@@ -69,23 +76,43 @@ public class Hangman extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TextView resp = (TextView) findViewById(R.id.response);
-                EditText input = (EditText) findViewById(R.id.textView);
+                input.setOnClickListener(this);
                 String guess = input.getText().toString();
-                initialABC.setText("");
-                abc = changeABC(guess, abc);
-                resp.setText(abc);
-                // changes the word into underscores
-                newUnderScores = guessResults(guess, word, newUnderScores);
-                TextView phrase = (TextView) findViewById(R.id.phraseid);
-                phrase.setText(newUnderScores);
-                counter = checkWinOrLose(guess, word, counter);
-                TextView numWrong = (TextView) findViewById(R.id.numWrong);
-                numWrong.setText("Number Wrong: " + counter);
-                if (counter >= 5) {
-                    gameOver();
+                input.setText("");
+
+                if (guess.length() >= 1) { // changes a word to the first letter
+                    guess = guess.charAt(0) + "";
+                    guess = guess.toUpperCase();
+                }
+                if (!(guess.length() < 1 || !abc.contains(guess) || newUnderScores.contains(guess))) {
+                    guess = guessCheck(guess, abc, newUnderScores, input);
+                    initialABC.setText("");
+                    abc = changeABC(guess, abc);
+                    resp.setText(abc);
+                    // changes the word into underscores
+                    newUnderScores = guessResults(guess, word, newUnderScores);
+                    TextView phrase = (TextView) findViewById(R.id.phraseid);
+                    phrase.setText(newUnderScores);
+                    counter = checkWinOrLose(guess, word, counter);
+                    TextView numWrong = (TextView) findViewById(R.id.numWrong);
+                    status = checkWinOrLoseString(guess, word);
+                    TextView statusMessage = (TextView) findViewById(R.id.statusMessage);
+                    statusMessage.setText(status);
+                    numWrong.setText("Number Wrong: " + counter);
+
+                    if (counter >= 5) {
+                        gameOver(false);
+                    }
+
+                    if (!newUnderScores.contains("*")) {
+                        gameOver(true);
+                    }
+                } else {
+                    TextView statusMessage = (TextView) findViewById(R.id.statusMessage);
+                    status = "Your guess was invalid. Try again.";
+                    statusMessage.setText(status);
                 }
             }
-
         });
     }
 
@@ -100,6 +127,23 @@ public class Hangman extends AppCompatActivity {
         }
         return underScores;
     }
+
+    public static String guessCheck(String guess, String abc, String phrase, EditText input) {
+        if (guess.length() > 1) { // changes a word to the first letter
+            guess = guess.charAt(0) + "";
+        }
+        // checks if the guess is at least one character, part of the alphabet,
+        // or hasn't already been
+        // guessed
+        if (guess.length() < 1 || !abc.contains(guess) || phrase.contains(guess) || guess.equals("_")) {
+            guess = input.getText().toString();
+            guess = guess.toUpperCase();
+            //guessCheck(guess, abc, phrase, input);
+
+        }
+        return guess;
+    }
+
 
     // changes the current phrase to insert the guess if it matches a letter in
     // the secret phrase
@@ -126,7 +170,6 @@ public class Hangman extends AppCompatActivity {
     public static String guessResults(String guess, String secretPhrase, String currentPhrase) {
         if (secretPhrase.contains(guess)) {
             currentPhrase = newCurrentPhrase(guess, secretPhrase, currentPhrase);
-        } else {
         }
         return currentPhrase;
     }
@@ -181,11 +224,30 @@ public class Hangman extends AppCompatActivity {
         return counter;
     }
 
-    //this is where the game ends
-    public void gameOver() {
-        Button b = (Button) this.findViewById(R.id.button1);
-        b.setEnabled(false);
+    public static String checkWinOrLoseString(String guess, String phrase) {
+        if (!phrase.contains(guess)) {
+            return "Incorrect Answer. Try Again.";
+        }
+        return "Correct Answer! Good job.";
     }
 
+    //this is where the game ends
+    public void gameOver(boolean win) {
+        Button b = (Button) this.findViewById(R.id.button1);
+        b.setEnabled(false);
+        if (win) {
+            youHaveWon();
+        } else {
+            youHaveLost();
+        }
+    }
 
+    public void youHaveWon() {
+        TextView youWin = (TextView) findViewById(R.id.youWin);
+        youWin.setText("You Won!!!");
+    }
+
+    public void youHaveLost() {
+
+    }
 }
